e8a49ab387ceaa96cdc0a968dbda8c6577a2620e
nourhan36 <nourhanibrahim2004@gmail.com>
2024-02-02 15:50:37 +0200

    + sebha design and code

--- app/src/main/java/com/route/islamic39/ui/home/tasbeh/TasbehFragment.kt
+++ app/src/main/java/com/route/islamic39/ui/home/tasbeh/TasbehFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.route.islamic39.R
 import com.route.islamic39.databinding.FragmentTasbehBinding
 
 class TasbehFragment : Fragment() {
 
     lateinit var viewBinding: FragmentTasbehBinding
+    var counter = 0
+    var currentDhikrIndex = 0
+    lateinit var azkarList: MutableList<String>
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,5 +21,27 @@ class TasbehFragment : Fragment() {
     ): View? {
         viewBinding = FragmentTasbehBinding.inflate(inflater, container, false)
         return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        azkarList = resources.getStringArray(R.array.azkarList).toMutableList()
+        viewBinding.dhikrTv.text = azkarList[currentDhikrIndex]
+        onSebhaClick()
+    }
+
+    private fun onSebhaClick() {
+        viewBinding.bodyOfSebhaImv.setOnClickListener {
+            viewBinding.bodyOfSebhaImv.rotation += (360 / 33).toFloat()
+            if (counter < 33)
+                counter++
+            else {
+                counter = 0
+                currentDhikrIndex =
+                    if (currentDhikrIndex < azkarList.size - 1) ++currentDhikrIndex else 0
+                viewBinding.dhikrTv.text = azkarList[currentDhikrIndex]
+            }
+            viewBinding.counterTv.text = counter.toString()
+        }
     }
 }
